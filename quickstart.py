18d7706d32820c78fd61e6d55c3866826644e487
limabeans <toomanytypos@gmail.com>
2015-12-28 10:34:31 -0800

    can calculate number of hours a week spent in class

--- quickstart.py
+++ quickstart.py
@@ -9,6 +9,21 @@ from oauth2client import tools
 
 import datetime
 
+def get_upcoming_10_events(service):
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -65,44 +80,65 @@ def get_cal_ids(service):
     return calendar_ids
 
 def get_events_list(service, cal_id):
+    events_lst = []
     page_token = None
     while True:
         events = service.events().list(calendarId=cal_id, pageToken=page_token).execute()
+        events_lst += events
         for event in events['items']:
             print(event['summary'])
         page_token = events.get('nextPageToken')
         if not page_token:
             break
+    return events_lst
 
 
-def get_upcoming_10_events(service):
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-        orderBy='startTime').execute()
-    events = eventsResult.get('items', [])
+def get_weeks_stats(service, cal_id):
+    secs = 0
+    events_lst = []
+    page_token = None
+    tmp = datetime.datetime(month=1, day=18, year=2016)
+    start_day = datetime.datetime(month=1, day=18, year=2016).isoformat()+'Z'
+    end_day = (tmp + datetime.timedelta(days=7)).isoformat()+'Z'
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    while True:
+        events = service.events().list(calendarId=cal_id, pageToken=page_token, timeMin=start_day, timeMax=end_day).execute()
+        events_lst += events
+        for event in events['items']:
+            s = event['start']['dateTime']
+            e = event['end']['dateTime']
+            s_d = datetime.datetime.strptime(s, '%Y-%m-%dT%H:%M:%S-08:00')
+            e_d = datetime.datetime.strptime(e, '%Y-%m-%dT%H:%M:%S-08:00')
+            secs += (e_d - s_d).total_seconds()
+            print(event['summary'], s, e)
+            
+        page_token = events.get('nextPageToken')
+        if not page_token:
+            break
+
+    print(secs/3600)
+    return events_lst
+
+    
+    
 
 
 def main():
-
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-
     calendar_ids = get_cal_ids(service)
-    print (get_events_list(service, calendar_ids['life']))
-
-
+    # print (get_events_list(service, calendar_ids['life']))
+    # print(calendar_ids['class'])
+    print('first weeks classes:')
+    get_weeks_stats(service, calendar_ids['class'])
+    # get_upcoming_10_events(service)
 if __name__ == '__main__':
     main()
+
+
+
 
 
 
